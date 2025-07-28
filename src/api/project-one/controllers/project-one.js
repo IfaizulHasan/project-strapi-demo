b69f45598eac12b9b16b4b5a7e472409e956a57a
IzuruSakamaki <schro406@gmail.com>
2025-07-28 09:35:20 +0700

    init

---
+++ src/api/project-one/controllers/project-one.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * project-one controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::project-one.project-one');
---
+++ src/api/project-one/routes/project-one.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * project-one router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::project-one.project-one');
